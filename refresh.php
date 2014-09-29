e5d213a5e281c0734245540051fcc2bc7ad8b136
Richard <nvreaver@gmail.com>
2014-09-29 11:43:15 -0700

    Update refresh.php

--- refresh.php
+++ refresh.php
@@ -433,10 +433,15 @@ $dv_rigs =  $dv_rigs - 1;
 												}
 											}
 											echo round(($devfreq / $c) , 0);
-											echo " & " ; 											
 											$myFile = "/var/www/conf/minerconfig.dat";
 											$lines = file($myFile);//file in to an array
-											echo $lines[2]; //line 3
+											if($lines[0] >= 0)
+											{
+												echo " & " ; 											
+											
+
+												echo $lines[2]; //line 3
+											}
 										}
 										else {
 											$myFile = "/var/www/conf/minerconfig.dat";
