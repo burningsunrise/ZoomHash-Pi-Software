f6a76e03f078bd8dffa14bdea75a18d63b806db0
Richard <nvreaver@gmail.com>
2014-10-01 11:47:08 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -8,6 +8,7 @@ sudo chown -R pi /var/www
 sudo chmod 777 /var/www/conf/minerconfig.dat
 sudo rm -rf /var/www/conf/miner_conf.json
 sudo rm -rf /var/www/ZoomHash-Pi-Software/
+sudo rm -rf /var/www/startup.sh
 
 echo "Your Pi will now reboot and you will need to re-enter your settings!"
 sudo shutdown -r now
